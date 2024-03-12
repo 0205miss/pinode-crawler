0853ed77da26bbf6539c88378ff389f2bca28bd9
lazytime <0205miss@gmail.com>
2024-03-12 16:26:48 +0800

    update

--- examples/crawl.js
+++ examples/crawl.js
@@ -1,53 +1,39 @@
+// @ts-nocheck
 const jsonStorage = require('../lib').jsonStorage;
 const blocked = require('blocked-at');
 const { QuorumSet } = require('@stellarbeat/js-stellarbeat-shared');
 const { createCrawler } = require('../lib');
-const { getConfigFromEnv } = require('@stellarbeat/js-stellar-node-connector');
+const nodes = require('../crawl_result/resultV1.json')
+
+const NetConfig = {
+	network: 'Pi Testnet',
+		nodeInfo: {
+			ledgerVersion: 15,
+			overlayMinVersion: 14,
+			overlayVersion: 15,
+			versionString: 'stellar-core 15.3.0 (6b99ef893c7f13f22c7c72a7f66ea912aeb4ad73)'
+		},
+		listeningPort: 31402,
+		privateKey: undefined,
+		receiveSCPMessages: true,
+		receiveTransactionMessages: true,
+}
 
 // noinspection JSIgnoredPromiseFromCall
 main();
 
 async function main() {
-	if (process.argv.length <= 2) {
-		console.log('Usage: ' + __filename + ' NODES.JSON_PATH ');
-
-		process.exit(-1);
-	}
-	let nodesJsonPath = process.argv[2];
-
-	console.log('[MAIN] Reading NODES.JSON_PATH');
-	let nodes = await jsonStorage.getNodesFromFile(nodesJsonPath);
-
 	console.log('[MAIN] Crawl!');
-	let trustedQSet = new QuorumSet(15, [
-		'GCVJ4Z6TI6Z2SOGENSPXDQ2U4RKH3CNQKYUHNSSPYFPNWTLGS6EBH7I2',
-		'GCIXVKNFPKWVMKJKVK2V4NK7D4TC6W3BUMXSIJ365QUAXWBRPPJXIR2Z',
-		'GBLJNN3AVZZPG2FYAYTYQKECNWTQYYUUY2KVFN2OUKZKBULXIXBZ4FCT',
-		'GD5QWEVV4GZZTQP46BRXV5CUMMMLP4JTGFD7FWYJJWRL54CELY6JGQ63',
-		'GDXQB3OMMQ6MGG43PWFBZWBFKBBDUZIVSUDAZZTRAWQZKES2CDSE5HKJ',
-		'GCFONE23AB7Y6C5YZOMKUKGETPIAJA4QOYLS5VNS4JHBGKRZCPYHDLW7',
-		'GA7TEPCBDQKI7JQLQ34ZURRMK44DVYCIGVXQQWNSWAEQR6KB4FMCBT7J',
-		'GA5STBMV6QDXFDGD62MEHLLHZTPDI77U3PFOD2SELU5RJDHQWBR5NNK7',
-		'GCMSM2VFZGRPTZKPH5OABHGH4F3AVS6XTNJXDGCZ3MKCOSUBH3FL6DOB',
-		'GA7DV63PBUUWNUFAF4GAZVXU2OZMYRATDLKTC7VTCG7AU4XUPN5VRX4A',
-		'GARYGQ5F2IJEBCZJCBNPWNWVDOFK7IBOHLJKKSG2TMHDQKEEC6P4PE4V',
-		'GC5SXLNAM3C4NMGK2PXK4R34B5GNZ47FYQ24ZIBFDFOCU6D4KBN4POAE',
-		'GBJQUIXUO4XSNPAUT6ODLZUJRV2NPXYASKUBY4G5MYP3M47PCVI55MNT',
-		'GAK6Z5UVGUVSEK6PEOCAYJISTT5EJBB34PN3NOLEQG2SUKXRVV2F6HZY',
-		'GD6SZQV3WEJUH352NTVLKEV2JM2RH266VPEM7EH5QLLI7ZZAALMLNUVN',
-		'GAZ437J46SCFPZEDLVGDMKZPLFO77XJ4QVAURSJVRZK2T5S7XUFHXI2Z',
-		'GADLA6BJK6VK33EM2IDQM37L5KGVCY5MSHSHVJA4SCNGNUIEOTCR6J5T',
-		'GCM6QMP3DLRPTAZW2UZPCPX2LF3SXWXKPMP3GKFZBDSF3QZGV2G5QSTK',
-		'GCGB2S2KGYARPVIA37HYZXVRM2YZUEXA6S33ZU5BUDC6THSB62LZSTYH',
-		'GABMKJM6I25XI4K7U6XWMULOUQIQ27BCTMLS6BYYSOWKTBUXVRJSXHYQ',
-		'GAYXZ4PZ7P6QOX7EBHPIZXNWY4KCOBYWJCA4WKWRKC7XIUS3UJPT6EZ4',
-		'GAVXB7SBJRYHSG6KSQHY74N7JAFRL4PFVZCNWW2ARI6ZEKNBJSMSKW7C',
-		'GAAV2GCVFLNN522ORUYFV33E76VPC22E72S75AQ6MBR5V45Z5DWVPWEU'
+
+	let topTierQSet = new QuorumSet(51, [
+		'GC6R2IQ7LAEWFFNV2ZMXPMOLGGCFRFX6NQFCNT2PLB3KVJPHTIYV4ZPR',
+		'GDRIZZC5PNZ6XBJTBZX52WX4NQ2K4Y5XBKVBRYNFDDS4VO6POQ3IGZCL',
+		'GAKI7FNXWIJHKV4CKFSU6KDNFTPYAUOP2WIKL5IFSOPEUSYK4Y3LWMXX'
 	]);
 
 	let myCrawler = createCrawler({
-		nodeConfig: getConfigFromEnv(),
-		maxOpenConnections: 70,
+		nodeConfig: NetConfig,
+		maxOpenConnections: 300,
 		maxCrawlTime: 900000,
 		blackList: new Set()
 	});
@@ -62,13 +48,13 @@ async function main() {
 			.map((node) => [node.ip, node.port]);
 
 		const topTierAddresses = nodes
-			.filter((node) => trustedQSet.validators.includes(node.publicKey))
+			.filter((node) => topTierQSet.validators.includes(node.publicKey))
 			.map((node) => [node.ip, node.port]);
 
 		let result = await myCrawler.crawl(
 			addresses,
 			topTierAddresses,
-			trustedQSet,
+			topTierQSet,
 			{
 				sequence: BigInt(0),
 				closeTime: new Date(0)
